4a161da721c3d3f3e1d34d1786bbf6cf2dda539c
Ziheng Sun <zsun@gmu.edu>
2020-04-14 16:37:09 -0400

    update

--- js/covid.js
+++ js/covid.js
@@ -1,4 +1,83 @@
 
+/**
+ * 
+ * COVID Risk Calculation
+ * 
+ * Author: Jensen Ziheng Sun
+ * Date: 4/14/2020
+ * 
+ * Some rights reserved. 
+ * 
+ */
+
+var zipcodecsv = "data/uszips.csv"
+
+var covidcsv = "data/04-13-2020.csv"
+
+var popcsv = "data/co-est2019-alldata.csv"
+
+function getPopByFIPS(fipscode){
+
+    fipscode = Number(fipscode)
+
+    $.ajax({
+        
+        type: "GET",
+        
+        url: popcsv,
+        
+        dataType: "text",
+
+        success: function(data) {
+            
+            var record_num = 15;  // or however many elements there are in each row
+            var allTextLines = allText.split(/\r\n|\n/);
+            var entries = allTextLines[0].split(',');
+            var lines = [];
+
+            var headings = entries.splice(0,record_num);
+            while (entries.length>0) {
+                var tarr = [];
+                for (var j=0; j<record_num; j++) {
+                    tarr.push(headings[j]+":"+entries.shift());
+                }
+                lines.push(tarr);
+            }
+
+            console.log(lines)
+        
+        }
+     });
+
+    
+
+}
+
+function getCOVIDByFIPS(fipscode){
+
+    fipscode = Number(fipscode)
+
+
+
+}
+
+function getPopByZip(zipcode){
+
+    zipcode = Number(zipcode)
+
+
+
+}
+
+function getCOVIDByZip(zipcode){
+
+    zipcode = Number(zipcode)
+
+
+
+
+}
+
 function riskestimate(total_population, store_people_count, potential_covid_cases){
 
     // calculate no clash probability
@@ -31,5 +110,20 @@ $("#calc").click(function(){
 
     $("#results_own").html("<p>Estimation:</p>"+
     "<p>The probability of meeting people with COVID in the grocery stores/gyms/restaurants/workplaces/recreational areas is <bold style=\"color:red\">" +  risk + "%</bold>.</p>")
+
+})
+
+$("#findfips").click(function(){
+
+    var fipscode = $("#fipscode").val()
+
+    // var zipcode = $("#zipcode").val()
+
+    var popret = getPopByFIPS(fipscode);
+
+    var covidret = getCOVIDByFIPS(fipscode);
+
+    // https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-13-2020.csv
+
 
 })
